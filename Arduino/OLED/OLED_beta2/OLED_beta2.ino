7736fb5a495379b9c6220885d5854a4adf23b5b7
Christopher Paretti <chris.paretti@gmail.com>
2013-04-29 23:06:25 -0700

    Working MiniArduino

--- Arduino/OLED/OLED_beta2/OLED_beta2.ino
+++ Arduino/OLED/OLED_beta2/OLED_beta2.ino
@@ -2,10 +2,10 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define OLED_DC 11
+#define OLED_DC 9
 #define OLED_CS 12
-#define OLED_CLK 10
-#define OLED_MOSI 9
+#define OLED_CLK 8
+#define OLED_MOSI 7
 #define OLED_RESET 13
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
@@ -43,12 +43,14 @@ static unsigned char PROGMEM logo16_glcd_bmp[] =
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-String yo = "Hello";
+int pwr = 2;
+
 int test = 0;
 
 void setup()   {                
   Serial.begin(57600);
-  
+  pinMode(pwr, OUTPUT);
+  digitalWrite(pwr, HIGH);
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
   // init done
